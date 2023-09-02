79a3095495250b358b420643b8264164f7474c3a
dg-Tech1 <130319167+dg-Tech1@users.noreply.github.com>
2023-09-02 17:18:52 +0200

    accordéon modif collapse.jsx

--- src/components/Collapse/collapse.jsx
+++ src/components/Collapse/collapse.jsx
@@ -1,19 +1,19 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef } from 'react'  // Import de react et hooks pour les besoins du composant
 import open from '../../assets/ChevronOpen.png'
 import './collapse.css'
 
 
-export default function Collapse (props) {
-  const [setActive, setActiveState] = useState("");
-  const [setHeight, setHeightState] = useState("0px");
-  const [setRotate, setRotateState] = useState("open");
-  const content = useRef(null);
+export default function Collapse (props) {    // exporte le composant 'Collapse' et usestate pour Créer 3 variables
+  const [setActive, setActiveState] = useState(""); // 'setActive, soit "", soit éffondré, soit étendu
+  const [setHeight, setHeightState] = useState("0px"); // la hauteur soit 0px éffondré soit la hauteur du contenu
+  const [setRotate, setRotateState] = useState("open"); // Orientation image btn fermé/ouvert
+  const content = useRef(null); // hook useRef pour créer une référence à l'élément de contenu du composant.
   
-  function toggleCollapse() {
-    setActiveState(setActive === "" ? "active" : "");
-    setHeightState(setActive === "active" ? "0px" : `${content.current.scrollHeight}px`);
-    setRotateState(setRotate === "open" ? "close" : "open");
-  } 
+  function toggleCollapse() {  // toggleCollapse quand on clique sur le bouton
+    setActiveState(setActive === "" ? "active" : ""); //
+    setHeightState(setActive === "active" ? "0px" : `${content.current.scrollHeight}px`); //
+    setRotateState(setRotate === "open" ? "close" : "open"); //
+  } // // // Ces lignes mettent à jour l'état du composant.
   return (
         <div className="collapse_div">
           <button className={`collapse_btn ${setActive}`} onClick={toggleCollapse}  >
