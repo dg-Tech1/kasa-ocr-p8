aa0197346d074b0f27604bc004658abb854d7337
dg-Tech1 <130319167+dg-Tech1@users.noreply.github.com>
2023-09-02 17:48:52 +0200

    hooks index.jsx

--- src/utils/hooks/index.jsx
+++ src/utils/hooks/index.jsx
@@ -1,14 +1,14 @@
-import { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react' // import des hooks, gérer les composants et effet de cotés.
 
 //appel api avec loader
-export function useFetch(url) {
-  const [data, setData] = useState({})
-  const [isLoading, setLoading] = useState(true)
-  const [error, setError] = useState(false)
+export function useFetch(url) { // export de l'url intérogée.
+  const [data, setData] = useState({}) // état qui sera utilisé pour stocker les données API
+  const [isLoading, setLoading] = useState(true) // état en cours de chargement, affichage indicateur de chargement....
+  const [error, setError] = useState(false) // affichage des erreurs le cas échéants
 
-  useEffect(() => {
-    if (!url) return
-    setLoading(true)
+  useEffect(() => { //Ce code utilise le hook useEffect pour effectuer une requête à une adresse web 
+    if (!url) return //spécifique. Il gère l'état du chargement, stocke les données récupérées et signale les 
+    setLoading(true)//erreurs. Les résultats sont renvoyés sous forme d'objet contenant ces états.
     async function fetchData() {
       try {
         const response = await fetch(url)
